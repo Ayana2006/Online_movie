1e62d666c272be228c30e1e932773d91ba2e590b
Ayana2006 <borubaevaayan@gmail.com>
2023-07-17 18:26:38 +0600

    100%

--- cinema/settings.py
+++ cinema/settings.py
@@ -140,12 +140,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-OSEMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = config.email
-EMAIL_HOST_PASSWORD = config.email_password
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config.EMAIL_HOST
+EMAIL_PORT = config.EMAIL_PORT
+EMAIL_HOST_USER = config.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = config.EMAIL_HOST_PASSWORD
+EMAIL_USE_TLS = config.EMAIL_USE_TLS
 
 
 STATIC_URL = '/static/'
